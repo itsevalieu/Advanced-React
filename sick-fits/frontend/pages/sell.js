f488da0684b18a21ac7116fb1f55d08549595f65
Eva Lieu <itsevalieu@gmail.com>
2018-12-09 14:13:24 -0800

    Setting up components and pages

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+// class Home extends React.Component {
+//     render() {
+//         return <p>Heyyy!</p>
+//     }
+// }
+import Link from 'next/link';
+const Sell = props => (
+    <div>
+        <p>Sell!</p>
+    </div>
+);
+
+export default Sell;
